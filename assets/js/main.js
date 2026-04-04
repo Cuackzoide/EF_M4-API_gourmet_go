9823a2718ee305d0191e7d88214491470181437a
Oliver Cuackzoide <barraoliver94@gmail.com>
2026-04-04 20:56:38 -0300

    feat: implement recipe modal functionality and add main.js logic for data fetching and rendering

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,6 +28,16 @@ function formatter(string) {
   const strValue = string.toLowerCase().trim();
   return removeAccents(strValue);
 }
+// Funcion para formatear instrucciones
+function splitInstructions(instructions) {
+  if (!instructions) return "";
+  return instructions
+    .split(/\r?\n/)
+    .filter(step => step.trim() !== "")
+    .map(step => `<li>${step.trim()}</li>`)
+    .join("");
+}
+
 // Array de ingredientes buscados, con valores por defecto para inicio rapido
 let searchHistory = [
   "onion",
@@ -67,8 +77,8 @@ function showRecipes(recipesArray, elementHTML) {
           <div class="card-body d-flex flex-column">
           <h5 class="card-title">${product.strMeal}</h5>
           <p class="card-text">${product.idMeal}</p>
-          <button class="btn btn-primary mt-auto">
-          GO to Recipe
+          <button type="button" class="btn btn-primary recipe-btn" data-bs-toggle="modal" data-bs-target="#recipeModal" data-id="${product.idMeal}">
+            GO to Recipe
           </button>
           </div>
           </div>
@@ -89,6 +99,48 @@ function showTabs(categoriesArray, TabsBar) {
     </li>`;
   });
 }
+
+// Funcion para renderizar una receta completa
+function fullRecipe(recipe, elementHTML) {
+  elementHTML.innerHTML = "";
+  if (!recipe || recipe.length === 0) {
+    elementHTML.innerHTML = `<p class="text-center w-100 lead">No recipes found.</p>`;
+    return;
+  }
+  let ingredientsHTML = "";
+  for (let i = 1; i <= 20; i++) {
+    const ingredient = recipe["strIngredient" + i];
+    const measure = recipe["strMeasure" + i];
+    if (ingredient && ingredient.trim() !== "") {
+      ingredientsHTML += `<li>${ingredient} - ${measure}</li>`;
+    }
+  }
+
+  elementHTML.innerHTML += `
+      <div class="modal-header">
+        <h1 class="modal-title text-center fs-5" id="recipeModalLabel">
+        ${recipe.strMeal}
+        </h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <img src="${recipe.strMealThumb}" alt="${recipe.strMeal}" class="img-thumbnail mb-3">
+        <div class="border rounded p-2 mb-2">
+        <h2>Ingredients</h2>
+        <ul>
+          ${ingredientsHTML}
+        </ul>
+        </div>
+        <div class="border rounded p-2 mb-2">
+        <h2>Instructions</h2>
+        <ul class="list-unstyled">${splitInstructions(recipe.strInstructions)}</ul>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>`;
+}
+
 // Función para buscar ingredientes válidos desde la API
 async function searchIngredient() {
   const url = "https://www.themealdb.com/api/json/v1/1/list.php?i=list";
@@ -113,21 +165,28 @@ async function searchMeals(ingredient) {
     return [];
   }
 }
+
+async function searchRecipeById(id) {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+  try {
+    const response = await fetch(url);
+    const recipe = await response.json();
+    return recipe;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
+
 // Selección de elementos del DOM
 const searchForm = document.querySelector('form[role="search"]');
 const searchInput = document.getElementById("searchInput");
 const recipeContainer = document.querySelector("#recipe-container");
 const navTabs = document.querySelector(".nav-tabs");
 const navBrand = document.querySelector(".navbar-brand");
-
-// Carga inicial de la página
-document.addEventListener("DOMContentLoaded", async (e) => {
-  showTabs(searchHistory, navTabs);
-  showRecipes(await searchMeals(searchHistory[0]), recipeContainer);
-  const links = document.querySelectorAll(".nav-link");
-  links.forEach((el) => el.classList.remove("active"));
-  if (links[0]) links[0].classList.add("active");
-});
+const modalContent = document.querySelector("#modal-content");
+const modalElement = document.getElementById("recipeModal");
+const modal = new bootstrap.Modal(modalElement);
 
 // Manejo del envío de la busqueda
 searchForm.addEventListener("submit", async (e) => {
@@ -162,7 +221,6 @@ navTabs.addEventListener("click", async (e) => {
   const links = document.querySelectorAll(".nav-link");
   links.forEach((el) => el.classList.remove("active"));
   link.classList.add("active");
-
   const ingredient = e.target.textContent.trim();
   const recipes = await searchMeals(ingredient);
   showRecipes(recipes, recipeContainer);
@@ -171,6 +229,27 @@ navTabs.addEventListener("click", async (e) => {
 // Manejo del clic en la "Brand" de la barra de navegación
 navBrand.addEventListener("click", async (e) => {
   e.preventDefault();
+  showTabs(searchHistory, navTabs);
+  showRecipes(await searchMeals(searchHistory[0]), recipeContainer);
+  const links = document.querySelectorAll(".nav-link");
+  links.forEach((el) => el.classList.remove("active"));
+  if (links[0]) links[0].classList.add("active");
+});
+
+// Manejo del clic en el botón de la receta 
+// Seleccionamos recipeContainer para usar event delegation
+recipeContainer.addEventListener("click", async (e) => {
+  e.preventDefault();
+  const recipeBtn = e.target.closest(".recipe-btn");
+  if (!recipeBtn) return;
+  const recipeId = recipeBtn.dataset.id;
+  const recipe = await searchRecipeById(recipeId);
+  fullRecipe(recipe.meals[0], modalContent);
+  modal.show();
+});
+
+// Carga inicial de la página
+document.addEventListener("DOMContentLoaded", async (e) => {
   showTabs(searchHistory, navTabs);
   showRecipes(await searchMeals(searchHistory[0]), recipeContainer);
   const links = document.querySelectorAll(".nav-link");
